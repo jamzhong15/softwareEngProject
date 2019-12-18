1a04d8bc5559467e4ed8b5483e8dc002c7767fa1
xiaoqi tan <zcabxta@ucl.ac.uk>
2019-12-18 12:55:14 +0000

    added cd, ls, pwd test (ls no argument test fail dont know reason)

--- src/main/java/uk/ac/ucl/jsh/Jsh.java
+++ src/main/java/uk/ac/ucl/jsh/Jsh.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
