67677165781191f7305cfdf9125abf3902c50f54
xiaoqi tan <zcabxta@ucl.ac.uk>
2019-12-20 22:12:40 +0000

    try to fix bug about mvn package

--- src/test/java/uk/ac/ucl/jsh/TailTest.java
+++ src/test/java/uk/ac/ucl/jsh/TailTest.java
@@ -2,22 +2,17 @@ package uk.ac.ucl.jsh;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.io.PrintStream;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.io.Writer;
 import java.util.Scanner;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -25,16 +20,39 @@ import org.junit.rules.ExpectedException;
 
 public class TailTest {
     // tail 1 filename argument test something wrong here
+    @Before
+    public void createNewFile() throws IOException {
+        String filePath = System.getProperty("user.dir") + File.separator + "testFile.txt";
+        File file = new File(filePath);
+        file.createNewFile();
+        FileWriter fileWriter = new FileWriter(file);
+        String data1 = "first line\n";
+        String data2 = "second line";
+        fileWriter.write(data1);
+        fileWriter.write(data2);
+        fileWriter.close();
+    }
+ 
+    @After
+    public void deleteFile() {
+        String filePath = System.getProperty("user.dir") + File.separator + "testFile.txt";
+        File file = new File(filePath);
+        file.delete();
+    }
+
     @Test
     public void TailOneFileNameArgumentTest() throws Exception {
         PipedInputStream in = new PipedInputStream();
         PipedOutputStream out;
         out = new PipedOutputStream(in);
-        Jsh.start("tail p.txt", out);
+        Jsh.start("tail testFile.txt", out);
         Scanner scn = new Scanner(in);
-        assertEquals(scn.nextLine(), "hello there");
+        assertEquals(scn.nextLine(), "first line");
+        assertEquals(scn.nextLine(), "second line");
         scn.close();
     }
+
+
 
     // tail with 3 arguments test something wrong also
     @Test
@@ -42,9 +60,9 @@ public class TailTest {
         PipedInputStream in = new PipedInputStream();
         PipedOutputStream out;
         out = new PipedOutputStream(in);
-        Jsh.start("tail -n 1 p.txt", out);
+        Jsh.start("tail -n 1 testFile.txt", out);
         Scanner scn = new Scanner(in);
-        assertEquals(scn.nextLine(), "Hi there");
+        assertEquals(scn.nextLine(), "second line");
         scn.close();
     }
 
