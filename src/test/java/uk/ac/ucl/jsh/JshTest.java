0c3446414a1bd21cfb4da1ef9fa68c2a0d5137da
Yingming Luo <Yingming.luo@gmail.com>
2019-12-20 13:41:31 +0000

    changing test

--- src/test/java/uk/ac/ucl/jsh/JshTest.java
+++ src/test/java/uk/ac/ucl/jsh/JshTest.java
@@ -1,22 +1,22 @@
 package uk.ac.ucl.jsh;
 
-import org.junit.Test;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
+// import org.junit.Test;
+// import org.junit.runner.JUnitCore;
+// import org.junit.runner.Result;
+// import org.junit.runner.notification.Failure;
 
 
 public class JshTest {
 
-    @Test
-    public void runTest() throws Exception
-    {
-        Result result = JUnitCore.runClasses(JshTestSuite.class);
+    // @Test
+    // public void runTest() throws Exception
+    // {
+    //     Result result = JUnitCore.runClasses(JshTestSuite.class);
 
-        for (Failure failure : result.getFailures()) {
-            System.out.println(failure.toString());
-        }
-        System.out.println(result.wasSuccessful());
-    }
+    //     for (Failure failure : result.getFailures()) {
+    //         System.out.println(failure.toString());
+    //     }
+    //     System.out.println(result.wasSuccessful());
+    // }
 }
 
