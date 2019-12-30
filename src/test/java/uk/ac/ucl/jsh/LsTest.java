6ac11ff32cc510d68e6910a6c0fdb8bb882a81af
James Zhong <jamzhong@gmail.com>
2019-12-30 11:59:57 +0000

    ls update

--- src/test/java/uk/ac/ucl/jsh/LsTest.java
+++ src/test/java/uk/ac/ucl/jsh/LsTest.java
@@ -18,89 +18,89 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LsTest {
-    // Jsh jsh = new Jsh();
+    Jsh jsh = new Jsh();
 
-    // @Rule
-    // public TemporaryFolder folder = new TemporaryFolder();
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
-    // @Before
-    // public void buildTestFile() throws IOException
-    // {
-    //     jsh.setcurrentDirectory(folder.getRoot().getAbsolutePath());
-    //     File src_folder = folder.newFolder("src");
-    //     File test_folder = folder.newFolder("src", "test");
-    //     File main_folder = folder.newFolder("src", "main");
-    //     src_folder.mkdirs();
-    //     test_folder.mkdirs();
-    //     main_folder.mkdirs();
-    // }
+    @Before
+    public void buildTestFile() throws IOException
+    {
+        jsh.setcurrentDirectory(folder.getRoot().getAbsolutePath());
+        File src_folder = folder.newFolder("src");
+        File test_folder = folder.newFolder("src", "test");
+        File main_folder = folder.newFolder("src", "main");
+        src_folder.mkdirs();
+        test_folder.mkdirs();
+        main_folder.mkdirs();
+    }
 
-    // @After
-    // public void deleteTestFile()
-    // {
-    //     jsh.setcurrentDirectory(System.getProperty("user.dir"));
-    //     folder.delete();
-    // }
+    @After
+    public void deleteTestFile()
+    {
+        jsh.setcurrentDirectory(System.getProperty("user.dir"));
+        folder.delete();
+    }
 
-    // @Test
-    // public void hi() throws Exception
-    // {
-    //     jsh.start("ls", System.out);
-    //     jsh.start("cd src ; ls", System.out);
-    // }
+    @Test
+    public void hi() throws Exception
+    {
+        jsh.start("ls", System.out);
+        jsh.start("cd src ; ls", System.out);
+    }
     
-    // // ls no argument test
-    // @Test
-    // public void lsWithoutArgument() throws Exception {
-    //     String currentDirectory = jsh.getcurrentDirectory();
-    //     File currDir = new File(currentDirectory);
-    //     ArrayList<String> listFiles = new ArrayList<>();
-    //     File[] listOfFiles = currDir.listFiles();
+    // ls no argument test
+    @Test
+    public void lsWithoutArgument() throws Exception {
+        String currentDirectory = jsh.getcurrentDirectory();
+        File currDir = new File(currentDirectory);
+        ArrayList<String> listFiles = new ArrayList<>();
+        File[] listOfFiles = currDir.listFiles();
 
-    //     PipedInputStream in = new PipedInputStream();
-    //     PipedOutputStream out = new PipedOutputStream(in);
-    //     jsh.start("ls", out);
-    //     Scanner scn = new Scanner(in);
-    //     String contentString = scn.nextLine();
-    //     String[] files = contentString.split("\t");
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        jsh.start("ls", out);
+        Scanner scn = new Scanner(in);
+        String contentString = scn.nextLine();
+        String[] files = contentString.split("\t");
         
-    //     // obtaining expected files names
-    //     for (File file : listOfFiles) {
-    //         if (!file.getName().startsWith(".")) 
-    //         {
-    //             listFiles.add(file.getName().toString());
-    //         }
-    //     }
-    //     for (String fileName : files)
-    //     {
-    //         assertTrue("wrong files displayed", listFiles.contains(fileName));
-    //     }
-    //     scn.close();
-    // }
+        // obtaining expected files names
+        for (File file : listOfFiles) {
+            if (!file.getName().startsWith(".")) 
+            {
+                listFiles.add(file.getName().toString());
+            }
+        }
+        for (String fileName : files)
+        {
+            assertTrue("wrong files displayed", listFiles.contains(fileName));
+        }
+        scn.close();
+    }
 
-    // //ls one argument test
-    // @Test
-    // public void lsWithOneArgument() throws Exception {
-    //     Jsh jsh = new Jsh();
-    //     ArrayList<String> folders = new ArrayList<>();
-    //     folders.add("test");
-    //     folders.add("main");
+    //ls one argument test
+    @Test
+    public void lsWithOneArgument() throws Exception {
+        Jsh jsh = new Jsh();
+        ArrayList<String> folders = new ArrayList<>();
+        folders.add("test");
+        folders.add("main");
 
-    //     PipedInputStream in = new PipedInputStream();
-    //     PipedOutputStream out = new PipedOutputStream(in);
-    //     jsh.start("ls src", out);
-    //     Scanner scn = new Scanner(in);
-    //     String[] files = scn.nextLine().split("\t");
-    //     for (String fileName : files)
-    //     {
-    //         assertTrue("wrong files displayed", folders.contains(fileName));
-    //     }
-    //     scn.close();
-    // }
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        jsh.start("ls src", out);
+        Scanner scn = new Scanner(in);
+        String[] files = scn.nextLine().split("\t");
+        for (String fileName : files)
+        {
+            assertTrue("wrong files displayed", folders.contains(fileName));
+        }
+        scn.close();
+    }
 
-    // // // too many arguments test
-    // @Rule
-    // public ExpectedException thrown = ExpectedException.none();
+    // // too many arguments test
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     // @Test
     // public void lsTooManyArgumentsThrowsException() throws RuntimeException, IOException {
@@ -112,14 +112,14 @@ public class LsTest {
     //     jsh.start("ls arg1 arg2", console);
     // }
 
-    // // no such directory test
-    // @Test
-    // public void lsNoSuchDirectoryThrowsException() throws RuntimeException, IOException {
-    //     Jsh jsh = new Jsh();
-    //     PrintStream console = null;
-    //     console = System.out;
-    //     thrown.expect(RuntimeException.class);
-    //     thrown.expectMessage(CoreMatchers.equalTo("ls: no such directory"));
-    //     jsh.start("ls xxx", console);
-    // }
+    // no such directory test
+    @Test
+    public void lsNoSuchDirectoryThrowsException() throws RuntimeException, IOException {
+        Jsh jsh = new Jsh();
+        PrintStream console = null;
+        console = System.out;
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage(CoreMatchers.equalTo("ls: not an existing directory"));
+        jsh.start("ls xxx", console);
+    }
 }
